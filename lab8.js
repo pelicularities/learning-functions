101d8ebc0477cd3fa68b17c6d3f074966d52ef8c
elsonlim <elsonlim7@gmail.com>
2019-08-28 14:45:14 +0800

    add recussive

--- lab8.js
+++ lab8.js
@@ -1,1 +1,13 @@
+const assert = require("assert");
+
 // questions from: https://www.codewars.com/kata/functional-addition
+// Create a function add(n)
+// Add(n) which returns a function that always adds n to any number
+
+/**
+ * var addOne = add(1);
+ * addOne(3); // 4
+ *
+ * var addThree = add(3);
+ * addThree(3); // 6
+ */

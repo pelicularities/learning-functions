b21264164d620d1a06e490ae8258aad2d5ebfc09
elsonlim <elsonlim7@gmail.com>
2019-09-06 15:42:41 +0800

    another bind, call and apply example on lab5

--- lab5.js
+++ lab5.js
@@ -6,7 +6,43 @@ function says(message1, message2) {
 }
 
 // using call get the assertion to pass
-assert(says() === "Peter says hello and welcome", "true");
+// object
+assert(
+  says.call({ name: "Peter" }, "hello", "welcome") ===
+    "Peter says hello and welcome",
+  "true"
+);
 
 // using apply get the assertion to pass
-assert(says() === "Peter says hello and welcome", "true");
+// object, array
+assert(
+  says.apply({ name: "Peter" }, ["hello", "welcome"]) ===
+    "Peter says hello and welcome",
+  "true"
+);
+
+const iceCreamMachine = {
+  make: function(topping) {
+    let flavour = this.flavour;
+    if (topping) {
+      flavour += " " + topping;
+    }
+    return flavour;
+  },
+};
+
+function IceCream(flavour) {
+  this.flavour = flavour;
+}
+
+const chocolateIceCream = new IceCream("chocolate"); // Todo: create a chocolate Ice cream
+const makeChocolateIceCream = iceCreamMachine.make.bind(chocolateIceCream); // create a make method with chocolate ice cream
+
+assert(makeChocolateIceCream() === "chocolate", "should return chocolate");
+
+const vanilaIceCream = new IceCream("vanilla");
+assert(
+  iceCreamMachine.make.call(vanilaIceCream, "sprinkle-nuts") ===
+    "vanilla sprinkle-nuts",
+  "should return vanila with sprinkle-nuts"
+);

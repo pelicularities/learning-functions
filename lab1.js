4b1d782fc27aa39f7ebeb36258d3e71f7e66eaa5
Mabel Lee <yanhwa.lee@thoughtworks.com>
2020-03-31 11:47:31 +0800

    [mabel] Modify lab1 questions

--- lab1.js
+++ lab1.js
@@ -1,21 +1,43 @@
 const assert = require("assert");
-// Assert out values from the function
 
 // 1. write a function that returns the area of a triangle
 function getAreaOfTriangle(base, height) {
-  return 0.5 * base * height;
+  // fill in the function code
 }
+
 assert(
   getAreaOfTriangle(3, 8) === 12,
   "area of triangle with base of 3 and height of 8 should be 12"
 );
 
+// 2. write a function that returns the area of a square
+// with the `length` parameter with a default value 5
+
+// function getAreaOfSquare...
+
+assert(
+  getAreaOfSquare(10) === 100,
+  "area of square with length of 10 should be 100"
+);
+
+assert(
+  getAreaOfSquare() === 25,
+  "area of square with length of 5 (default value) should be 25"
+);
+
 // 2. using rest parameters, return the smallest number
-function getSmallest(...rest) {}
+function getSmallest(...rest) {
+  // fill in the function code
+}
 
 assert(
   getSmallest(1, 2, 3, 4, 6, 7, 8) === 1,
-  "should return  the smallest number"
+  "should return the smallest number"
+);
+
+assert(
+  getSmallest(100, 100, 8, 8) === 8,
+  "should return the smallest number even with duplicates"
 );
 
 // 3. Create a IIFE and that console.log a hello text immediately

ca938dab44a8f8a13e8a9999c493640cad1bed1d
Grace Teng <pelicularities@users.noreply.github.com>
2021-01-20 14:15:41 +0800

    complete 1-basics.js

--- lab1.js
+++ lab1.js
@@ -3,6 +3,7 @@ const assert = require("assert");
 // 1. write a function that returns the area of a triangle
 function getAreaOfTriangle(base, height) {
   // fill in the function code
+  return 0.5 * base * height;
 }
 
 assert(
@@ -14,6 +15,9 @@ assert(
 // with the `length` parameter with a default value 5
 
 // function getAreaOfSquare...
+function getAreaOfSquare(length = 5) {
+  return length ** 2;
+}
 
 assert(
   getAreaOfSquare(10) === 100,
@@ -28,6 +32,13 @@ assert(
 // 3. using rest parameters, return the smallest number
 function getSmallest(...rest) {
   // TODO: your code here
+  let min = rest[0];
+  for (let i = 1; i < rest.length; i++) {
+    if (rest[i] < min) {
+      min = rest[i];
+    }
+  }
+  return min;
 }
 
 assert(
@@ -41,3 +52,6 @@ assert(
 );
 
 // 4. Create a IIFE and that console.log a hello text immediately
+(function() {
+  console.log('hello');
+})();
